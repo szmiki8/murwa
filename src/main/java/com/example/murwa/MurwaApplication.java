87e975d68389159a43cd207b7653895794c8d2c3
Miklos Szabo <szmiki8@gmail.com>
2018-04-08 20:44:45 +0200

    spring boot started project (spring initializer)

---
+++ src/main/java/com/example/murwa/MurwaApplication.java
@@ -0,0 +1,12 @@
+package com.example.murwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurwaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/murwa/MurwaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.murwa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MurwaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
